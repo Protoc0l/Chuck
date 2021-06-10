796fd77ff344d5933006ee0f1e530930c93c6556
Protoc0l <carbonmanifesto@gmail.com>
2021-06-10 08:52:26 -0600

    Delete Python-Delete-File.py

--- Modules/ToDoList Viewer/Maxpat/Python-Delete-File.py
+++
@@ -1,4 +0,0 @@
-import os
-os.remove('C:/Users/corym/OneDrive/Desktop/SLE Photos/05-21-2020 22-06-32 Identification 1.png')
-os.remove('C:/Users/corym/OneDrive/Desktop/SLE Photos/05-21-2020 22-06-32 Identification 1.hek')
-print('Files Removed!') 
